249bef40e80dd80c82df5aaa34b1be338ecb5cb7
Aleksei Abdulin <alexexiv@gmail.com>
2026-06-17 15:42:19 +0500

    Initial Commit

---
+++ RouterSwiftUI/RouterSwiftUI.swift
@@ -0,0 +1,9 @@
+//
+//  RouterSwiftUI.swift
+//  RouterSwiftUI
+//
+//  Created by ALEXEY ABDULIN on 17.06.2026.
+//
+
+import Foundation
+
---
+++ RouterSwiftUITests/RouterSwiftUITests.swift
@@ -0,0 +1,19 @@
+//
+//  RouterSwiftUITests.swift
+//  RouterSwiftUITests
+//
+//  Created by ALEXEY ABDULIN on 17.06.2026.
+//
+
+import Testing
+@testable import RouterSwiftUI
+
+struct RouterSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
