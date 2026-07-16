5d22c2aed45ad72a5b563695f0ccb30c4d0fbfb0
Aleksei Abdulin <alexexiv@gmail.com>
2026-07-16 12:58:26 +0500

    Refactor

--- RouterExample/Screens/Main/MainViewModel.swift
+++ RouterExample/Screens/Main/MainViewModel.swift
@@ -10,46 +10,16 @@ import Combine
 @MainActor
 protocol MainViewModel: ObservableObject
 {
-    associatedtype FlowerListVM: FlowerListViewModel
-    associatedtype AddFlowerVM: AddFlowerViewModel
-    associatedtype CartVM: CartViewModel
-    
-    var selectedTab: AppTab { get set }
-    
-    func MakeFlowerListViewModel() -> FlowerListVM
-    func MakeAddFlowerViewModel() -> AddFlowerVM
-    func MakeCartViewModel() -> CartVM
+
 }
 
 final class MainViewModelStub: MainViewModel
 {
-    @Published var selectedTab: AppTab = .home
-    
-    func MakeFlowerListViewModel() -> FlowerListViewModelStub
-    {
-        return FlowerListViewModelStub(
-            onCartRequested:
-            { [weak self] in
-                self?.selectedTab = .cart
-            }
-        )
-    }
-    
-    func MakeAddFlowerViewModel() -> AddFlowerViewModelStub
-    {
-        return AddFlowerViewModelStub()
-    }
-    
-    func MakeCartViewModel() -> CartViewModelStub
-    {
-        return CartViewModelStub()
-    }
+
 }
 
 final class MainViewModelImpl: MainViewModel
 {
-    @Published var selectedTab: AppTab = .home
-    
     private let flowerService: FlowerServiceProtocol
     private let cartService: CartServiceProtocol
     
@@ -57,29 +27,5 @@ final class MainViewModelImpl: MainViewModel
     {
         self.flowerService = flowerService
         self.cartService = cartService
-    }
-    
-    func MakeFlowerListViewModel() -> FlowerListViewModelImpl
-    {
-        return FlowerListViewModelImpl(
-            flowerService: flowerService,
-            cartService: cartService,
-            onCartRequested: SelectCartTab
-        )
-    }
-    
-    func MakeAddFlowerViewModel() -> AddFlowerViewModelImpl
-    {
-        return AddFlowerViewModelImpl( flowerService: flowerService )
-    }
-    
-    func MakeCartViewModel() -> CartViewModelImpl
-    {
-        return CartViewModelImpl( cartService: cartService )
-    }
-    
-    private func SelectCartTab()
-    {
-        selectedTab = .cart
     }
 }
